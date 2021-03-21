6ac5fa6bdc0e78f8d9ba0379da0af609166d7fe8
Dima Matroskin <dima.matroskin062@gmail.com>
2021-03-21 21:57:46 +0200

    fixes and improvments

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { Image, Nav, Navbar, Dropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useFacebookAuth } from '../../hooks/useFacebookAuth';
@@ -9,7 +10,7 @@ const Navigation = () => {
   const { login, user, authenticated, logout } = useFacebookAuth();
 
   return (
-    <Navbar className={styles.border}>
+    <Navbar collapseOnSelect expand='md' className={styles.border}>
       <NavLink to='/'>
         <Navbar.Brand>
           <Image
@@ -17,55 +18,61 @@ const Navigation = () => {
             alt='icon'
             width='30'
             height='30'
-            className='mr-2 align-center align-top'
+            className={classNames('mr-2', 'align-center', 'align-top')}
             rounded
           />
         </Navbar.Brand>
       </NavLink>
-      <Nav className='mr-auto'>
-        <Nav.Link
-          as={NavLink}
-          to='/applications'
-          activeClassName='text-primary'
-        >
-          Applications
-        </Nav.Link>
-        <Nav.Link as={NavLink} to='/guide' activeClassName='text-primary'>
-          Guide
-        </Nav.Link>
-        <Nav.Link as={NavLink} to='/docs' activeClassName='text-primary'>
-          Docs
-        </Nav.Link>
-      </Nav>
-
-      <Dropdown className='mr-3'>
-        {!authenticated ? (
-          <>
-            <Dropdown.Toggle variant='primary'>Login with</Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={login}>Facebook</Dropdown.Item>
-              <Dropdown.Item>Google</Dropdown.Item>
-            </Dropdown.Menu>
-          </>
-        ) : (
-          <>
-            <Dropdown.Toggle variant='light'>
-              <Image
-                src={user.picture.data.url}
-                alt={user.name}
-                width='30'
-                h='30'
-                roundedCircle
-                className='mr-2'
-              />
-              {user.name}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
-            </Dropdown.Menu>
-          </>
-        )}
-      </Dropdown>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link
+            as={NavLink}
+            to='/applications'
+            activeClassName='text-primary'
+          >
+            Applications
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/guide' activeClassName='text-primary'>
+            Guide
+          </Nav.Link>
+          <Nav.Link as={NavLink} to='/docs' activeClassName='text-primary'>
+            Docs
+          </Nav.Link>
+        </Nav>
+        <Dropdown className='mr-3'>
+          {!authenticated ? (
+            <>
+              <Dropdown.Toggle variant='primary'>Login with</Dropdown.Toggle>
+              <Dropdown.Menu
+                className={classNames(styles.dropdown, 'dropdown-menu-right')}
+              >
+                <Dropdown.Item onClick={login}>Facebook</Dropdown.Item>
+                <Dropdown.Item>Google</Dropdown.Item>
+              </Dropdown.Menu>
+            </>
+          ) : (
+            <>
+              <Dropdown.Toggle variant='light'>
+                <Image
+                  src={user.picture.data.url}
+                  alt={user.name}
+                  width='30'
+                  h='30'
+                  roundedCircle
+                  className='mr-2'
+                />
+                {user.name}
+              </Dropdown.Toggle>
+              <Dropdown.Menu
+                className={classNames(styles.dropdown, 'dropdown-menu-right')}
+              >
+                <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
+              </Dropdown.Menu>
+            </>
+          )}
+        </Dropdown>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- src/hooks/useFacebookAuth.jsx
+++ src/hooks/useFacebookAuth.jsx
@@ -12,6 +12,15 @@ export const useFacebookAuth = () => {
         version: 'v10.0',
       });
     };
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
   }, []);
 
   const login = () => {
