6d22477ff4eec3556b5aaeaa38f2e4bf5ac9fdbd
Dima Matroskin <dima.matroskin062@gmail.com>
2021-03-27 17:16:49 +0200

    private routes

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { isAuthenticatedSelector } from './PrivateRouter.selector';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
+  return (
+    <Route
+      {...rest}
+      render={() => (isAuthenticated ? <Component /> : <Redirect to='/' />)}
+    />
+  );
+};
+
+export default PrivateRoute;
